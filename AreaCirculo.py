ee5f9858327681f0ce8552bf4788402a5b807e74
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-07 23:43:15 -0500

    Create AreaCirculo.py

---
+++ AreaCirculo.py
@@ -0,0 +1,4 @@
+radio = int(input("Introduce la radio del circulo: "))
+areaCirculo = 3.1415 * radio * radio
+circunferencia = 2 * 3.1415 * radio
+print(f"Area = {areaCirculo} y circunferencia = {circunferencia}")
