c8f8635d506c2ca78824d272fa62158f97f4946f
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-08 00:05:27 -0500

    Create CastInt.py

---
+++ CastInt.py
@@ -0,0 +1,8 @@
+'''Hacer Cast con int'''
+
+floatAInt = int(3.5)
+stringAInt = int("1")
+
+print(f"Resultado de convertir de float a int es: {floatAInt}")
+print(f"Resultado de convertir de string a int es: {stringAInt}")
+
