9f76a5a90c27f2378de6e1367e7e7fbb302e4cfe
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-14 00:28:14 -0500

    Create CastFloat.py

---
+++ CastFloat.py
@@ -0,0 +1,4 @@
+int_a_float    = float(4)
+string_a_float = float("1") #El numero es tratado como string
+print(f"Despues de que el entero se le haya hecho cast a float, el resultado es: {int_a_float}")
+print(f"Despues de que el string se le haya hecho cast a float, el resultado es: {string_a_float}")
