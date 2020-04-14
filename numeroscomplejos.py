6f4108b7cb283d7880b00fd7852d95da3bb90c64
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-14 00:36:32 -0500

    Create numeroscomplejos.py

---
+++ numeroscomplejos.py
@@ -0,0 +1,4 @@
+complejo_a_string = complex("1")
+complejo_a_numero = complex(5, 8)
+print(f"Resultado despues de usar un string en la parte real del numero complejo {complejo_a_string}")
+print(f"Resultado despues de usar numeros en la parte real e imaginaria del numero complejo {complejo_a_numero}")
