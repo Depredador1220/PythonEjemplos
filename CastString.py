ef3e0aeaf02971afda1d9b3c4d0f7d0272338560
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-04-14 00:31:32 -0500

    Create CastString.py

---
+++ CastString.py
@@ -0,0 +1,4 @@
+int_a_string    = str(8)
+float_a_string  = str(3.5) 
+print(f"Despues de que el entero se le haya hecho cast con string, el resultado es: {int_a_string}")
+print(f"Despues de que el float se le haya hecho cast con string, el resultado es: {float_a_string}")
