97b1cf0846ec283ddf3ddf6d601eeff332eb6b23
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-05-01 15:41:01 -0500

    Create PareImpar.py

---
+++ PareImpar.py
@@ -0,0 +1,6 @@
+"""Programa para determinar si un numero es par o impar"""
+numero = int(input("Introduce un numero: "))
+if numero % 2 == 0:
+    print(f"{numero} es par")
+else:
+    print(f"{numero} es inpar")
