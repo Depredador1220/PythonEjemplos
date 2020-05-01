6735ca2959228a4c36256250380d2ee1b3baca82
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-05-01 15:44:03 -0500

    Create MayoryMenorComparacion.py

---
+++ MayoryMenorComparacion.py
@@ -0,0 +1,9 @@
+"""Programa para determinar el mayor de dos numeros"""
+
+numero1 = int(input("Introduce el primer numero: "))
+numero2 = int(input("Introduce el segundo numero: "))
+
+if numero1 > numero2:
+    print(f"{numero1} es mayor que {numero2}")
+else:
+    print(f"{numero2} es mayor que {numero1}")
