907376e3c4bcabc48f4b656f53a16301e5ab70f6
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-06-01 02:03:40 -0500

    Create AreaTrapecio.py

---
+++ AreaTrapecio.py
@@ -0,0 +1,11 @@
+"""Area Trapecio"""
+
+def Area_Trapecio(a, b, h):
+    area = 0.5 * (a + b) * h
+    print(f"El area del trapecio es {area}")
+
+def main():
+    Area_Trapecio(10, 15, 20)
+
+if __name__ == "__main__":
+    main()
