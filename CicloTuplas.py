b48380f5641abc34753ce1d83b11c74d147b791c
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-06-20 23:52:33 -0500

    Create CicloTuplas.py

---
+++ CicloTuplas.py
@@ -0,0 +1,9 @@
+"""Iterar con tuplas"""
+animalesOceanicos = ("Anguila electrica", "Medusa", "Camaron", "Tortuga", "Ballena")
+
+def main():
+    for each_animal in animalesOceanicos:
+        print(f"{each_animal} es un animal del mar")
+
+if __name__ == "__main__":
+    main()
